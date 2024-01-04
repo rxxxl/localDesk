964c2895cd7b92e603cdd8bf9de7e2ef202d6100
Raul Mendoza <mpr1931126069@gmail.com>
2024-01-04 16:54:07 -0600

    Add login page and related images

--- module/Auth/AuthView.php
+++ module/Auth/AuthView.php
@@ -5,47 +5,87 @@ class AuthView
 {
     public function login($message)
     {
-        $login = file_get_contents("./public/html/Auth/login.html");
+        $head = file_get_contents("./public/html/Auth/head.html");
+        $login = file_get_contents("./public/html/Auth/loginv2.html");
 
         $data = array(
-            "MESSAGE" => $message
+            "MESSAGE" => $message, 
+            "title" => "Login"
         );
+
+        $headData = array(
+            "title" => "Login",
+            "bootstrap" => "/public/bootstrap/css/bootstrap.min.css",
+            "css_file" => "/public/css/login.css",
+            "js_file" => "/public/js/loginValidation.js"
+        );
+
+        $template = new Template($head);
+        $head = $template->render($headData);
 
         $template = new Template($login);
         $login = $template->render($data);
+
+        echo $head;
         echo $login;
+        
     }
 
     public function forgotPassword($message)
     {
-
-        $forgotPassword = file_get_contents("./public/html/Auth/forgotPassword.html");
+        $head = file_get_contents("./public/html/Auth/head.html");
+        $forgotPassword = file_get_contents("./public/html/Auth/forgotPasswordv2.html");
 
         $data = array(
-            "MESSAGE" => $message
+            "MESSAGE" => $message,
+            "title" => "Forgot password"
         );
+
+        $headData = array(
+            "title" => "Login",
+            "bootstrap" => "/public/bootstrap/css/bootstrap.min.css",
+            "css_file" => "/public/css/login.css",
+            "js_file" => "/public/js/loginValidation.js"
+        );
+
+        $template = new Template($head);
+        $head = $template->render($headData);
+
         $tempalte = new Template($forgotPassword);
         $forgotPassword = $tempalte->render($data);
 
+        echo $head;
         echo $forgotPassword;
     }
 
     public function resetPasswordForm($message, $user)
     {
-        $resetPasswordForm = file_get_contents("./public/html/Auth/resetPasswordForm.html");
+        $head = file_get_contents("./public/html/Auth/head.html");
+        $resetPasswordForm = file_get_contents("./public/html/Auth/resetPasswordFormv2.html");
 
         $email = $user["email"];
         $token = $user["token"];
         $data = array(
-
+            "title" => "Reset password",
             "email" => $email,
             "token" => $token,
             "MESSAGE" => $message,
             "js_file" => "/public/js/passwordValidation.js"
         );
+
+        $headData = array(
+            "title" => "Login",
+            "bootstrap" => "/public/bootstrap/css/bootstrap.min.css",
+            "css_file" => "/public/css/login.css"
+        );
+
+        $template = new Template($head);
+        $head = $template->render($headData);
+
         $tempalte = new Template($resetPasswordForm);
         $resetPasswordForm = $tempalte->render($data);
 
+        echo $head;
         echo $resetPasswordForm;
     }
 
